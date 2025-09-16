9b6af0271c37631bbf0b568bb9f604d1d5bf9c61
minhok05 <minhokin05091950@gmail.com>
2025-09-15 22:36:47 -0700

    Created first.java

---
+++ first.java
@@ -0,0 +1,13 @@
+// src/main/java/com/example/App.java
+
+package com.example;
+
+public class App {
+    public String getGreeting() {
+        return "Hello world!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
